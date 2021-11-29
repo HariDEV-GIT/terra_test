8fb24bdbf0732030ebeaf90f1136e184a10c29d7
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2021-11-29 16:39:52 +0530

    Update file

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
-         aws =          {
+    aws = {
       source  = "hashicorp/aws"
-          version = "~> 3.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "aws" {
-        region = "us-east-1"
+  region = "us-east-1"
 }
 
-     resource "aws_instance" "web" {
-  ami  "ami-04902260ca3d33422"
-      instance_type =     "t2.micro"
+
+ resource "aws_instance" "web" {
+  ami           = "ami-04902260ca3d33422"
+  instance_type = "t2.micro"
 
   tags = {
-    Name =            "Hello World!"
+    Name = "HelloWorld"
   }
-}
